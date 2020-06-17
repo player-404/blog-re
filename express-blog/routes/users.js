11d2ceb2e263cb76b256fe769c839dc7aab93413
player-404 <1315363446@qq.com>
2020-06-17 23:07:06 +0800

    登录完成

--- express-blog/routes/users.js
+++ express-blog/routes/users.js
@@ -1,19 +1,38 @@
 var express = require('express');
 let login = require('../controller/user');
-const user = require('../controller/user');
+let { ErrorModel, SuccessModel } = require('../model/resmodel');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   let username = req.username;
   let password = req.password;
-  let result = login(username, password);
+  let result = login(username, password);  // session
   return result.then((data) => {
     if (data.username) {
       req.session.username = data.username;
+      res.json(
+        new SuccessModel('登录成功')
+      )
+      return
     }
+    res.json(
+      new ErrorModel('用户登录失败')
+    )
   })
 
-});
+})
+router.get('/login-test', (req, res, next) => {
+  if (req.session.username) {
+    res.json(
+      new SuccessModel('用户已登录')
+
+    )
+    return;
+  }
+  res.json(
+    new ErrorModel('用户尚未登录')
+  )
+})
 
 module.exports = router;
